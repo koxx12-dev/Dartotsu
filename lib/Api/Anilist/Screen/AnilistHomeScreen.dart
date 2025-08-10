6c4fbebcaf000178aa20b4784784007ebdd6690e
Ankit Grai <ankigray@outlook.com>
2025-08-10 15:02:35 +0530

    [build.all] fix

--- lib/Api/Anilist/Screen/AnilistHomeScreen.dart
+++ lib/Api/Anilist/Screen/AnilistHomeScreen.dart
@@ -1,8 +1,6 @@
 import 'package:dartotsu/Services/Screens/BaseHomeScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_rx/src/rx_types/rx_types.dart';
-import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 
 import '../../../Adaptor/Media/Widgets/MediaSection.dart';
 import '../../../DataClass/Media.dart';
