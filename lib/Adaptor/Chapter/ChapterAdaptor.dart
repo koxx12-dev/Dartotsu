725c9e44d8938ba9dad9f41ff828c922b66e9f0e
aayush262 <aayushthakur262006@gmail.com>
2025-08-23 19:34:00 +0530

    [build.all] [clean]

--- lib/Adaptor/Chapter/ChapterAdaptor.dart
+++ lib/Adaptor/Chapter/ChapterAdaptor.dart
@@ -2,6 +2,7 @@ import 'package:dartotsu/Functions/Function.dart';
 import 'package:dartotsu/Screens/Manga/MangaReader/Reader.dart';
 import 'package:dartotsu_extension_bridge/dartotsu_extension_bridge.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 import '../../Animation/ScaleAnimation.dart';
@@ -69,29 +70,35 @@ class ChapterAdaptorState extends State<ChapterAdaptor> {
           shrinkWrap: true,
           itemCount: chapterList.length,
           itemBuilder: (context, index) {
-            return SlideAndScaleAnimation(
-              initialScale: 0.0,
-              finalScale: 1.0,
-              initialOffset: const Offset(1.0, 0.0),
-              finalOffset: Offset.zero,
-              duration: const Duration(milliseconds: 200),
-              child: GestureDetector(
-                onTap: () => onChapterClick(
-                  context,
-                  chapterList[index],
-                  widget.source,
-                  widget.mediaData,
-                  widget.onEpisodeClick,
-                ),
-                child: SizedBox(
-                  width: double.infinity,
-                  child: ChapterListView(
-                    chapter: chapterList[index],
-                    mediaData: widget.mediaData,
-                  ),
+            return GestureDetector(
+              onTap: () => onChapterClick(
+                context,
+                chapterList[index],
+                widget.source,
+                widget.mediaData,
+                widget.onEpisodeClick,
+              ),
+              child: SizedBox(
+                width: double.infinity,
+                child: ChapterListView(
+                  chapter: chapterList[index],
+                  mediaData: widget.mediaData,
                 ),
               ),
-            );
+            )
+                .animate()
+                .slide(
+                  begin: const Offset(0, -1),
+                  end: Offset.zero,
+                  curve: Curves.easeInOut,
+                  duration: const Duration(milliseconds: 200),
+                )
+                .scale(
+                  begin: const Offset(0.1, 0.1),
+                  end: const Offset(1.0, 1.0),
+                  curve: Curves.easeInOut,
+                  duration: const Duration(milliseconds: 400),
+                );
           },
         ),
       ),
